1b032e91116ae1da3f4b60bd051a04cc0e7d623f
Julieta Gazzin <j.gazzin@gmail.com>
2023-01-07 16:45:29 -0300

    03: correcciones estilos y tipografias en inicio / pasar a subpaginas

--- js-inicio.js
+++ js-inicio.js
@@ -2,11 +2,11 @@ console.log("hola mundo");
 
 const parrafo = document.querySelector("#info__inicio");
 
-const textoUno = "Juguetes fabricados en material simple pero resistente a la manipulación de niños de más de 3 años, que plantean una actividad inicial guiada para luego estimular la versatilidad y creatividad en el juego.";
+const textoUno = "Juguetes fabricados en material liviano y resistente a la manipulación de niños mayores de 3 años, que plantean una actividad inicial guiada para luego estimular la versatilidad y creatividad en el juego libre.";
 
-const textoDos = "Pensados con secuencias didácticas que acompañan y fortalecen los procesos de aprendizaje de la lectoescritura y comprensión lectora en la primera infancia, haciendo hincapié en el desarrollo de consciencia fonológica y motricidad fina.";
+const textoDos = "Pensados a partir de secuencias didácticas que acompañan y fortalecen los procesos de aprendizaje de la lectoescritura y comprensión lectora en la primera infancia.";
 
-const textoTres = "Cada juego cuenta con el acceso a una aplicación web con juegos desarrollados para acompañar a los niños en un acercamiento guiado con la tecnología."
+const textoTres = "Cada juego cuenta con acceso a una aplicación web con actividades desarrolladas para acompañar a los niños en un acercamiento guiado con la tecnología."
 
 const iconUno = document.querySelector("#icon--1");
 const iconDos = document.querySelector("#icon--2");
